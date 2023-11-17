4b88170c6196584913e1a1df61dcf026da28ad36
psw1305 <shshck5@gmail.com>
2023-11-17 09:13:36 +0900

    Solution 01
    - 짝수와 홀수 해결 코드 등록

---
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_1/01_EvenAndOdd.cs
@@ -0,0 +1,24 @@
+﻿namespace AlgorithmCodeKata.Level_1;
+
+/// <summary>
+/// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+/// </summary>
+
+public class Level_1_01
+{
+    public string solution(int num)
+    {
+        string answer = "";
+
+        if (num % 2 == 0)
+        {
+            answer = "Even";
+        }
+        else
+        {
+            answer = "Odd";
+        }
+
+        return answer;
+    }
+}
