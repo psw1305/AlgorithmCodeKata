edc6c0b079db15d54adc7e2b86f5b877435edcf0
psw1305 <shshck5@gmail.com>
2023-11-17 09:27:00 +0900

    Solution 02
    - 평균값 풀이 코드 등록

---
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_1/02_Average.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmCodeKata.Level_1;
+
+/// <summary>
+/// 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+/// </summary>
+
+public class _02_Average
+{
+    public double solution(int[] arr)
+    {
+        double answer = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            answer += arr[i];
+        }
+
+        answer /= arr.Length;
+
+        return answer;
+    }
+}
