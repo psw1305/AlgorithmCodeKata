794407b7ca92869507636b64b61fca4e25b5b00b
psw1305 <shshck5@gmail.com>
2023-11-23 09:02:30 +0900

    Update Level_1 Soluctions
    - 레벨 1 폴더 생성 및 클래스 생성
    - 레벨 폴더 정리

---
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_1/Level_01_Soluctions.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmCodeKata.Level_1;
+
+public class Level_01_Soluctions
+{
+    // 두 수의 차
+    public int Solution_1(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+}
