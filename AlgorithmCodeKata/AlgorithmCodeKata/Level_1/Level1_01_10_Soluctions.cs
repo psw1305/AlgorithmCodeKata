87e6d75bc3da3a089db3bb618ab241f527d90356
psw1305 <shshck5@gmail.com>
2023-11-23 09:34:06 +0900

    Update Level_1 Soluctions
    - 솔루션 문제 10개 해결
    - 폴더 정리

---
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_1/Level1_01_10_Soluctions.cs
@@ -0,0 +1,49 @@
+﻿namespace AlgorithmCodeKata.Level_1;
+
+public class Level_01_Soluctions
+{
+    // 두 수의 차
+    public int Solution_1(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+
+    // 두 수의 곱
+    public int Solution_2(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+
+    // 몫 구하기
+    public int Soluction_3(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 / num2;
+        return answer;
+    }
+    
+    // 나이 출력
+    public int Solution_4(int age)
+    {
+        int answer = 0;
+        answer = 2023 - age;
+        return answer;
+    }
+
+    // 숫자 비교하기
+    public int Solution_5(int num1, int num2)
+    {
+        if (num1 == num2) return 1;
+        else return -1;
+    }
+
+    // 두 수의 합
+    public int Solution_6(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
---
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_1/Level1_11_20_Soluctions.cs
@@ -0,0 +1,62 @@
+﻿namespace AlgorithmCodeKata.Level_1;
+
+public class Level1_11_20_Soluctions
+{
+    /// <summary>
+    /// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+    /// </summary>
+    public string Solution_11(int num)
+    {
+        string answer = "";
+
+        if (num % 2 == 0)
+        {
+            answer = "Even";
+        }
+        else
+        {
+            answer = "Odd";
+        }
+
+        return answer;
+    }
+
+    /// <summary>
+    /// 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+    /// </summary>
+    public double Solution_12(int[] arr)
+    {
+        double answer = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            answer += arr[i];
+        }
+
+        answer /= arr.Length;
+
+        return answer;
+    }
+
+    /// <summary>
+    /// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+    /// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+    /// </summary>
+    public int Solution_13(int n)
+    {
+        int answer = 0;
+
+        while (n != 0)
+        {
+            int remain;
+
+            remain = n % 10;
+
+            answer += remain;
+
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
