55a376267abee3013443319d5d5b6818033cf124
psw1305 <shshck5@gmail.com>
2023-11-23 11:50:15 +0900

    Update Level_2 Soluctions
    문제 해결
    - 약수의 합
    - 나머지가 1이 되는 작은 수 찾기
    - x만큼 간격이 있는 n개의 숫자

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_1/Level1_01_10_Soluctions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_1/Level1_01_10_Soluctions.cs
@@ -1,6 +1,6 @@
 ﻿namespace AlgorithmCodeKata.Level_1;
 
-public class Level_01_Soluctions
+public class Level1_01_10_Soluctions
 {
     // 두 수의 차
     public int Solution_1(int num1, int num2)
@@ -45,5 +45,48 @@ public class Level_01_Soluctions
     public int Solution_6(int num1, int num2)
     {
         return num1 + num2;
+    }
+
+    // 두 수의 나눗셈
+    public int Soluction_7(int num1, int num2)
+    {
+        return (num1 * 1000) / num2;
+    }
+
+    // 각도기
+    public int Soluction_8(int angle)
+    {
+        int answer = 0;
+        if (angle > 0 && angle < 90) answer = 1;
+        else if (angle == 90) answer = 2;
+        else if (angle > 90 && angle < 180) answer = 3;
+        else if (angle == 180) answer = 4;
+        return answer;
+    }
+
+    // 짝수의 합
+    public int Soluction_9(int n)
+    {
+        int answer = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (i % 2 == 0) answer += i;
+        }
+
+        return answer;
+    }
+
+    // 배열의 평균값
+    public double Soluction_10(int[] numbers)
+    {
+        double answer = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            answer += numbers[i];
+        }
+
+        return answer / numbers.Length;
     }
 }
--- AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_11_20_Soluctions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_11_20_Soluctions.cs
@@ -1,6 +1,6 @@
 ﻿namespace AlgorithmCodeKata.Level_2;
 
-public class Level1_11_20_Soluctions
+public class Level2_11_20_Soluctions
 {
     /// <summary>
     /// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
@@ -55,6 +55,47 @@ public class Level1_11_20_Soluctions
             answer += remain;
 
             n /= 10;
+        }
+
+        return answer;
+    }
+
+    // 약수의 합
+    public int Solution_14(int n)
+    {
+        int answer = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0) answer += i;
+        }
+        return answer;
+    }
+
+    // 나머지가 1이 되는 작은 수 찾기
+    public int Solution_15(int n)
+    {
+        int answer = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (n % i == 1)
+            {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    // x만큼 간격이 있는 n개의 숫자
+    public long[] Solution_16(int x, int n)
+    {
+        long[] answer = new long[n];
+        long num = x;
+
+        for (int i = 0; i < n; i++)
+        {
+            answer[i] = num;
+            num += x;
         }
 
         return answer;
