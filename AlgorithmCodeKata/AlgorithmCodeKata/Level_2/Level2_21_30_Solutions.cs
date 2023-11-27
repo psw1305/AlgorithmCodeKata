0f01f6d59f28c956ef1f66feacfc999af3024dbb
psw1305 <shshck5@gmail.com>
2023-11-27 09:31:08 +0900

    Update Level_2 Solutions
    - 서울에서 김서방 찾기
    - 나누어 떨어지는 숫자

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_21_30_Solutions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_21_30_Solutions.cs
@@ -47,4 +47,51 @@ public class Level2_21_30_Solutions
 
         return answer;
     }
+
+    // 콜라츠 추측
+    public int Solution_23(long num)
+    {
+        int count = 0;
+
+        while (num != 1 && count <= 500)
+        {
+            num = num % 2 == 0 ? num /= 2 : (num * 3) + 1;
+            count++;
+        }
+
+        return count <= 500 ? count : -1;
+    }
+
+    // 서울에서 김서방 찾기
+    public string Solution_24(string[] seoul)
+    {
+        string answer = "";
+
+        for (int i = 0; i < seoul.Length; i++)
+        {
+            if (seoul[i].Contains("Kim"))
+            {
+                answer = $"김서방은 {i}에 있다";
+            }
+        }
+
+        return answer;
+    }
+
+    // 나누어 떨어지는 숫자
+    public int[] Solution_25(int[] arr, int divisor)
+    {
+        int[] answer = arr.Where(num => num % divisor == 0).ToArray();
+
+        if (answer.Length != 0)
+        {
+            Array.Sort(answer);
+        }
+        else
+        {
+            answer = new int[] { -1 };
+        }
+
+        return answer;
+    }
 }
