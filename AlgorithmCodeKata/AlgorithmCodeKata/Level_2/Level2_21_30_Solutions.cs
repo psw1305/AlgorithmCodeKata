cb26448e6144eb1522de960216773382d5d5b8aa
psw1305 <shshck5@gmail.com>
2023-11-28 09:23:34 +0900

    Update Level_2 Solutions
    - 음양 더하기
    - 핸드폰 번호 가리기
    - 없는 숫자 더하기
    - 제일 작은 수 제거하기

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_21_30_Solutions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_21_30_Solutions.cs
@@ -94,4 +94,85 @@ public class Level2_21_30_Solutions
 
         return answer;
     }
+
+    // 음양 더하기
+    public int Solution_26(int[] absolutes, bool[] signs)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.Length; i++)
+        {
+            if (signs[i])
+            {
+                answer += absolutes[i];
+            }
+            else
+            {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+
+    // 핸드폰 번호 가리기
+    public string Solution_27(string phone_number)
+    {
+        string answer = "";
+        int lastFourLength = phone_number.Length - 4;
+
+        for (int i = 0; i < phone_number.Length; i++)
+        {
+            if (i < lastFourLength)
+            {
+                answer += "*";
+            }
+            else
+            {
+                answer += phone_number[i];
+            }
+        }
+
+        return answer;
+    }
+
+    // 없는 숫자 더하기
+    public int Solution_28(int[] numbers)
+    {
+        int answer = 45;
+        int sum = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        answer -= sum;
+        return answer;
+    }
+
+    // 제일 작은 수 제거하기
+    public int[] Solution_29(int[] arr)
+    {
+        int[] answer = new int[] { };
+
+        if (arr.Length == 1)
+        {
+            return answer = new int[] { -1 };
+        }
+
+        int temp = int.MaxValue;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < temp)
+            {
+                temp = arr[i];
+            }
+        }
+
+        answer = arr.Where(num => num != temp).ToArray();
+
+        return answer;
+    }
 }
