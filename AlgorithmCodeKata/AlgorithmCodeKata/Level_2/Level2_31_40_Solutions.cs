dda2eeb88734202d3848bf0a85cd7ad2c14cb62d
psw1305 <shshck5@gmail.com>
2023-11-30 10:42:51 +0900

    Update Level_2 Solutions
    - 내적
    - 약수의 개수와 덧셈
    - 문자열 내림차순으로 배치하기

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_31_40_Solutions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_2/Level2_31_40_Solutions.cs
@@ -16,4 +16,47 @@ public class Level2_31_40_Solutions
 
         return answer;
     }
+
+    // 내적
+    public int Solution_32(int[] a, int[] b)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+
+    // 약수의 개수와 덧셈
+    public int Solution_33(int left, int right)
+    {
+        int answer = 0;
+
+        for (int i = left; i <= right; i++)
+        {
+            int count = 0;
+
+            for (int k = 1; k <= i; k++)
+            {
+                if (i % k == 0)
+                    count++;
+            }
+
+            answer += count % 2 == 0 ? i : -i;
+        }
+
+        return answer;
+    }
+
+    // 문자열 내림차순으로 배치하기
+    public string Solution_34(string s)
+    {
+        char[] charArr = s.ToCharArray();
+        Array.Sort(charArr);
+        Array.Reverse(charArr);
+        return new string(charArr);
+    }
 }
