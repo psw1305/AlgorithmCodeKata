a4ff16a047bd149ad55da0224ea7c0963c3653ac
psw1305 <shshck5@gmail.com>
2023-12-06 09:14:08 +0900

    Update Level_3 Solution
    - 이상한 문자 만들기

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_3/Level3_41_45_Solutions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_3/Level3_41_45_Solutions.cs
@@ -2,4 +2,32 @@
 
 public class Level3_41_45_Solutions
 {
+    // 이상한 문자 만들기
+    public string Solution_41(string s)
+    {
+        string answer = "";
+        string[] strArr = s.Split();
+        int count = strArr.Length;
+
+        foreach (string str in strArr)
+        {
+            count--;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    answer += str[i].ToString().ToUpper();
+                }
+                else
+                {
+                    answer += str[i].ToString().ToLower();
+                }
+            }
+
+            if (count > 0) answer += " ";
+        }
+
+        return answer;
+    }
 }
