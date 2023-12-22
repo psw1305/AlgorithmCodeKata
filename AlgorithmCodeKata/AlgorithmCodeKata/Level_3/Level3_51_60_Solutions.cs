979436e49ef1b05bc5ec4143f3a750a64b7d17a4
psw1305 <shshck5@gmail.com>
2023-12-22 14:05:06 +0900

    Update Level_3 Solutions
    - 푸드 파이트 대회
    - 콜라 문제

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_3/Level3_51_60_Solutions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_3/Level3_51_60_Solutions.cs
@@ -2,4 +2,47 @@
 
 public class Level3_51_60_Solutions
 {
+    // 푸드 파이트 대회
+    public string Solution_51(int[] food)
+    {
+        string strFirst = "";
+
+        for (int i = 0; i < food.Length; i++)
+        {
+            int num = int.Parse(food[i].ToString());
+
+            if (num % 2 != 0)
+            {
+                num -= 1;
+            }
+
+            for (int j = 0; j < num / 2; j++)
+            {
+                strFirst += i.ToString();
+            }
+        }
+
+        char[] charArray = strFirst.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+
+        string answer = strFirst + "0" + reversed;
+        return answer;
+    }
+
+    // 콜라 문제
+    public int Solution_52(int a, int b, int n)
+    {
+        int answer = 0;
+
+        while (n >= a)
+        {
+            int remain = n % a;
+            int result = n / a;
+            answer += result * b;
+            n = (result * b) + remain;
+        }
+
+        return answer;
+    }
 }
