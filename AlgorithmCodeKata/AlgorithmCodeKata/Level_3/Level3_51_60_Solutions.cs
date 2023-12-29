0e41b32bc62fa372a5c8d8338d903cacade7ab8b
psw1305 <shshck5@gmail.com>
2023-12-29 17:48:21 +0900

    Update Level_3 Solutions
    - 과일 장수
    - 모의고사

--- AlgorithmCodeKata/AlgorithmCodeKata/Level_3/Level3_51_60_Solutions.cs
+++ AlgorithmCodeKata/AlgorithmCodeKata/Level_3/Level3_51_60_Solutions.cs
@@ -112,4 +112,74 @@ public class Level3_51_60_Solutions
 
         return "Yes";
     }
+
+    // 과일 장수
+    public int Solution_56(int k, int m, int[] score)
+    {
+        int answer = 0;
+
+        int[] numbers = score.Where(n => n <= k && n >= 1).OrderByDescending(n => n).ToArray();
+
+        for (int i = 0; i < score.Length; i += m)
+        {
+            int[] slice = numbers.Skip(i).Take(m).ToArray();
+
+            if (slice.Length == m)
+            {
+                int min = slice.Min();
+                answer += min * m;
+            }
+        }
+
+        return answer;
+    }
+
+    // 모의고사
+    public int[] Solution_57(int[] answers)
+    {
+        List<int> answer = new List<int>();
+        int[] bestScore = new int[3];
+
+        int[] student1 = { 1, 2, 3, 4, 5 };
+        int[] student2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+        int[] student3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+
+        for (int i = 0; i < answers.Length; i++)
+        {
+            if (answers[i] == student1[i % student1.Length])
+            {
+                num1 += 1;
+            }
+
+            if (answers[i] == student2[i % student2.Length])
+            {
+                num2 += 1;
+            }
+
+            if (answers[i] == student3[i % student3.Length])
+            {
+                num3 += 1;
+            }
+        }
+
+        bestScore[0] = num1;
+        bestScore[1] = num2;
+        bestScore[2] = num3;
+
+        int max = bestScore.Max();
+
+        for (int i = 0; i < bestScore.Length; i++)
+        {
+            if (bestScore[i] == max)
+            {
+                answer.Add(i + 1);
+            }
+        }
+
+        return answer.ToArray();
+    }
 }
